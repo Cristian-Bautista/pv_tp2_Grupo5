0a882d42926a26c4fcc2a1e7f27aad6747605321
Cristian-Bautista <cristianbautista1516@gmail.com>
2025-04-21 17:41:37 -0300

    ejercicio7

---
+++ js/ejercicio7.js
@@ -0,0 +1,10 @@
+const nombres=["Valeria","Mateo","Camila","Santiago","Isabella","Nicolás"];
+let nombreMasLargo = "";
+
+nombres.forEach((i) => {
+  if (i.length > nombreMasLargo.length) {
+    nombreMasLargo = i;
+  }
+});
+
+console.log("El nombre más largo es:", nombreMasLargo);
