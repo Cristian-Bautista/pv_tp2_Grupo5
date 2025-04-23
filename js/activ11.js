4eb19b7f8db8339c40f180483e9038b217124ec1
gabicalisaya <39198816@fi.unju.edu.ar>
2025-04-22 21:59:08 -0300

    trab

---
+++ js/activ11.js
@@ -0,0 +1,6 @@
+function calcular() {
+    const kilometro= document.getElementById("kilometro").value;
+    const litro= document.getElementById("litro").value;
+    const precioLitro= precio/litros;
+    alert("El precio por litro es: " + precioLitro);
+}
---
+++ js/activ3.js
@@ -0,0 +1,3 @@
+const n=7.5;
+const area= Math.PI;
+console.log('El area de el circulo es: ' + area*n);
